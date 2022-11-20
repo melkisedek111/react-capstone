004ad0a793c3980be2582d9fea89edc73a252fb4
melkisedek111 <melkisedek222@gmail.com>
2022-11-20 09:03:36 +0800

    removing unused imports

--- src/Admin/Modules/Dashboard/Dashboard.jsx
+++ src/Admin/Modules/Dashboard/Dashboard.jsx
@@ -1,16 +1,5 @@
 import { Box, Divider, Grid, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableData from "../TableData/TableData.jsx";
-import { useGetInquiriesQuery } from "../../../redux/api/inquire.api.js";
-import moment from "moment/moment.js";
+import React from "react";
 
 import BarChart from "./components/BarChart.jsx";
 import PieChart from "./components/PieChart.jsx";
